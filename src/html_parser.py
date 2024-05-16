07d5ca8a0b1c92b23a513c0f49182f96be4c88ef
Yan Pazdniakou <yanpozdniakou@live.com>
2024-05-16 23:08:44 +0200

    url builder

--- src/html_parser.py
+++ src/html_parser.py
@@ -1,2 +1,10 @@
-def get_stocks_from_html(url: str, company_codes: list[str]):
+def get_stocks_from_html(domain: str, companies: list[dict[str,str]]):
+    for company in companies:
+    # https://www.londonstockexchange.com/stock/HSBA/hsbc-holdings-plc/company-page
+        company_code = company['code'].upper()
+        company_name = company['name'].lower().replace(' ', '-')
+        url = domain + '/stock/' + company_code + '/' + company_name + '/company-page'
+        print(url)
+
+
     pass
--- src/main.py
+++ src/main.py
@@ -1,20 +1,21 @@
 from csv_parser import get_companies_from_csv
 from api_parser import get_stocks_from_api
+from html_parser import get_stocks_from_html
 from printer import print_as_table
 
 
 def main():
     companies_csv_path = 'input.csv'
     api_url = 'https://api.londonstockexchange.com/api/v1/pages?path=home'
-    html_url= 'https://www.londonstockexchange.com/stock/'
+    html_url= 'https://www.londonstockexchange.com'
 
     companies = get_companies_from_csv(companies_csv_path)
 
-    stocks = get_stocks_from_api(api_url, companies)
-    if not stocks: return 1 #TODO
-    print_as_table(stocks)
+    # stocks = get_stocks_from_api(api_url, companies)
+    # if not stocks: return 1 #TODO
+    # print_as_table(stocks)
 
-    # stocks = get_stocks_from_html(html_url, company_codes)
+    stocks = get_stocks_from_html(html_url, companies)
     return 0
 
 
