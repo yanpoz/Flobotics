29e641c7fee83fd55068502bebfc812fd9082680
Yan Pazdniakou <yanpozdniakou@live.com>
2024-05-17 02:41:47 +0200

    build_single

--- src/html_parser.py
+++ src/html_parser.py
@@ -1,19 +1,18 @@
 from requests_html import HTMLSession
 
 
-def build_urls(domain: str, companies: list[dict[str,str]]):
-    for company in companies:
-        company_code = company['stock code'].upper()
-        company_name = company['company name'].lower().replace(' ', '-')
-        url = domain + '/stock/' + company_code + '/' + company_name + '/company-page'
-        yield url
+def build_url(domain: str, company: dict[str,str]) -> str:
+    # for company in companies:
+    company_code = company['stock code'].upper()
+    company_name = company['company name'].lower().replace(' ', '-')
+    url = domain + '/stock/' + company_code + '/' + company_name + '/company-page'
+    return url
 
 
 def get_stocks_from_html(domain: str, companies: list[dict[str,str]]):
-    for url in build_urls(domain, companies):
+    for company in companies:
         session = HTMLSession()
-
-        # Send GET request and render JavaScript
+        url = build_url(domain, company)
         headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
         response = session.get(url, headers=headers)
         response.html.render(sleep=1, keep_page=True, scrolldown=1)
